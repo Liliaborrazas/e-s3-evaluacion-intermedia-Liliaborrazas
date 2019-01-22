e5797244d109635f2d7b3ed03df865a0f5ccbab7
lilia <liliaborrazas14@gmail.com>
2019-01-22 16:53:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Pokemon from './components/Pokemon';
+
 
 
 class App extends Component {
@@ -26,7 +26,6 @@ class App extends Component {
 
     return (
       <div className="App">
-      <Pokemon pokemon={this.pokemon}/>
       </div>
       
     );
